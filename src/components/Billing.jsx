e9a676a36723df358f737ed8502c7b9d4a5a0a97
erfunit <erfunghodoosi@gmail.com>
2023-04-12 23:06:22 +0430

    title updated

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
-const Billing = () => {
-  return <div>Billing</div>;
-};
+const Billing = () => (
+  <section id="proudc" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img
+        src={bill}
+        alt="billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+    </div>
+  </section>
+);
 
 export default Billing;
