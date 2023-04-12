ef514c3dadaa9dd2a179a8ba59b18551e8dce844
erfunit <erfunghodoosi@gmail.com>
2023-04-12 16:33:23 +0430

    initialize commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      className="font-bold text-3xl
+      text-red-600
+        w-full 
+        bg-red-300 p-6 
+        hover:bg-red-200 
+        transition-all 
+        cursor-pointer"
+    >
+      App
+    </div>
+  );
+};
+
+export default App;
